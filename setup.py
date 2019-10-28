7f0c41e81d450977a3f848233b4ee643ee4f28c0
Thomas Kgaevski <thomas.kgaevski@gmail.com>
2019-10-28 17:42:52 +0100

    1.0.0 (#3)
    * Add mindsay client
    
    * Add replace_entity_type example
    
    * Add license (#1)
    
    * Add installation and usage to README (#2)
    
    * Reduce logo size in README
    
    * Update README
    
    * Add logger format placeholder
    
    * README fix by marketing team
    
    * Add CHANGELOG.md

---
+++ setup.py
@@ -0,0 +1,24 @@
+# pylint: skip-file
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mindsay-sdk',
+    version='0.1.0',
+    author='Mindsay tech team',
+    author_email='tech@mindsay.com',
+    description='Mindsay python client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Destygo/mindsay-sdk',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+    ],
+)
