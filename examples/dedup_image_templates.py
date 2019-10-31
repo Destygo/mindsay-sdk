69d221d90ea9e36b7c1636578d4c1d420f1f7237
Gratien Désormeaux <gratien@destygo.com>
2019-10-31 18:38:08 +0100

    cleaned file

--- examples/dedup_image_templates.py
+++ examples/dedup_image_templates.py
@@ -1,33 +1,26 @@
 """
-Mindsay SDK example to replace entity type
+client SDK example to replace entity type
 """
 import logging
 from collections import defaultdict
 
 import tqdm
-from mindsay_sdk import Client
+from client_sdk import Client
 
 
-logger = logging.getLogger("mindsay")
+logger = logging.getLogger("client")
 
 
-USER_EMAIL = "gratien@destygo.com"
-INSTANCE_NAME = "Benchmark NLP - test"
-LANGUAGE = "fr_FR"
-EXPERIMENT_ID = 15
-
-MINDSAY = Client(USER_EMAIL, production=True)
-
-
-def setup_bos_environment():
+def setup_bos_environment(user_email, instance_name, language, experiment_id):
     """Setup instance, language and experiment"""
-    instances = MINDSAY.get_instances()
+    client = Client(user_email, production=True)
+    instances = client.get_instances()
     instance = [
-        instance for instance in instances if instance["name"] == INSTANCE_NAME
+        instance for instance in instances if instance["name"] == instance_name
     ][0]
-    MINDSAY.set_current_instance(instance["id"])
-    MINDSAY.set_current_language(LANGUAGE)
-    MINDSAY.set_current_experiment(EXPERIMENT_ID)
+    client.set_current_instance(instance["id"])
+    client.set_current_language(language)
+    client.set_current_experiment(experiment_id)
 
 
 def _groupby_name(array):
@@ -38,33 +31,32 @@ def _groupby_name(array):
     return res
 
 
-def get_all_answers():
-    """Gather all answers content in a list"""
-    answer_record_ids = MINDSAY.get("/answers").json()
-
-    answers = []
-    for answer_record_id in answer_record_ids:
-        answer = MINDSAY.get("/answers/{}".format(answer_record_id["record_id"])).json()
-        answers.append(answer)
-
-    return answers
-
-
-def dedup_image_templates():
+def dedup_image_templates(client):
     """
     1. Group image templates by name
     2. Assumes 'same name' = 'duplicate'
     3. Adjust depending templates to point on image_template #1
     4. Keep image_template #1 and remove duplicates
     """
-    image_templates = MINDSAY.get("/image_templates").json()
+    image_templates = client.get("/image_templates").json()
+
     dupl_image_templates_by_name = {
         name: image_templates
         for name, image_templates in _groupby_name(image_templates).items()
         if len(image_templates) > 1
     }
     logger.info("Duplicated image_templates: %s", len(dupl_image_templates_by_name))
-    answers = get_all_answers()
+
+    def _get_all_answers(client):
+        """Gather all answers content in a list"""
+        answer_record_ids = client.get("/answers").json()
+
+        return [
+            client.get("/answers/{}".format(answer_record_id["record_id"])).json()
+            for answer_record_id in answer_record_ids
+        ]
+
+    answers = _get_all_answers(client)
     templates = [template for answer in answers for template in answer["templates"]]
 
     for image_template_name, image_templates_group in tqdm.tqdm(
@@ -82,7 +74,7 @@ def dedup_image_templates():
         for template in templates:
             if template["image_template_record_id"] in image_template_record_ids[1:]:
                 template["image_template_record_id"] = image_template_record_ids[0]
-                MINDSAY.put(
+                client.put(
                     f'/templates/{template["record_id"]}/',
                     json={"image_template_record_id": image_template_record_ids[0]},
                 )
@@ -93,7 +85,7 @@ def dedup_image_templates():
 
         # remove unused image_templates
         for image_template_record_id in image_template_record_ids[1:]:
-            MINDSAY.delete(f"/image_templates/{image_template_record_id}")
+            client.delete(f"/image_templates/{image_template_record_id}")
 
         logger.info(
             "%s deleted image_templates: %s",
@@ -102,9 +94,9 @@ def dedup_image_templates():
         )
 
 
-def is_image_templates_dedup():
+def is_image_templates_dedup(client):
     """Sanity check"""
-    image_templates = MINDSAY.get("/image_templates").json()
+    image_templates = client.get("/image_templates").json()
     assert not [
         image_templates
         for image_templates in _groupby_name(image_templates).values()
