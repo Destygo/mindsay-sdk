ebbfb5d596e619c5aba83c4215476a369a822fde
Anogio <9292868+Anogio@users.noreply.github.com>
2019-11-21 18:20:44 +0100

    Some utils + typing (#7)
    * Some utils + typing
    
    * Changes as per review
    
    * Remove parse_timestamp function

--- mindsay_sdk/client.py
+++ mindsay_sdk/client.py
@@ -7,6 +7,7 @@ import sys
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Optional
 
 import requests
 
@@ -48,7 +49,33 @@ class Client(requests.Session):
         self._user_sign_in(email)
         logger.warning("Connected to Mindsay on %s", environment)
 
-    def _user_sign_in(self, email: str):
+    @classmethod
+    def create_client(
+        cls,
+        user_email: str,
+        instance_name: str,
+        language: str,
+        production: bool,
+        experiment_id: Optional[int] = None,
+    ) -> "Client":
+        """Setup instance, language and experiment if specified"""
+        client = cls(user_email, production=production)
+
+        # Get the instance by name
+        instances = client.get_instances()
+        instance = [
+            instance for instance in instances if instance["name"] == instance_name
+        ][0]
+        client.set_current_instance(instance["id"])
+
+        client.set_current_language(language)
+
+        if experiment_id is not None:
+            client.set_current_experiment(experiment_id)
+
+        return client
+
+    def _user_sign_in(self, email: str) -> None:
         """
         Asks for the user password to log in and set Authorization
         header to the client
@@ -62,7 +89,7 @@ class Client(requests.Session):
         if response.json()["otp_required_for_login"]:
             self._user_code_auth(email)
 
-    def _user_code_auth(self, email: str):
+    def _user_code_auth(self, email: str) -> None:
         """
         Asks for user email code to authentify and add response
         cookies to the client
@@ -92,7 +119,7 @@ class Client(requests.Session):
         response.raise_for_status()
         return response.json()
 
-    def set_current_bot(self, bot_id: int):
+    def set_current_bot(self, bot_id: int) -> Dict[str, Any]:
         """Set the current bot"""
         response = self.post("environment/set_current_bot", json={"bot_id": bot_id})
         response.raise_for_status()
@@ -104,7 +131,7 @@ class Client(requests.Session):
         response.raise_for_status()
         return response.json()
 
-    def set_current_experiment(self, experiment_id: int):
+    def set_current_experiment(self, experiment_id: int) -> Dict[str, Any]:
         """Set the current experiment"""
         response = self.post(
             "environment/set_current_experiment", json={"experiment_id": experiment_id}
@@ -125,7 +152,7 @@ class Client(requests.Session):
         response.raise_for_status()
         return response.json()
 
-    def set_current_instance(self, instance_id: int):
+    def set_current_instance(self, instance_id: int) -> Dict[str, Any]:
         """Set the instance for next operations"""
         if self.production:
             instance = self.get_instance(instance_id)
@@ -141,15 +168,16 @@ class Client(requests.Session):
         logger.info(
             "Switched to instance %s", response.json()["current_instance"]["name"]
         )
+        return response.json()
 
-    def set_current_language(self, language: str):
+    def set_current_language(self, language: str) -> Dict[str, Any]:
         """Set the current environment language"""
         response = self.post("environment/change_language", json={"language": language})
         response.raise_for_status()
         logger.info("Current language updated: %s", language)
         return response.json()
 
-    def get_entity_type(self, entity_type_record_id: int) -> dict:
+    def get_entity_type(self, entity_type_record_id: int) -> Dict[str, Any]:
         """Returns the entity type matching the given id"""
         response = self.get(f"entity_types/{entity_type_record_id}")
         response.raise_for_status()
@@ -161,31 +189,35 @@ class Client(requests.Session):
         response.raise_for_status()
         return response.json()
 
-    def get_user_node(self, user_node_record_id: int) -> dict:
+    def get_user_node(self, user_node_record_id: int) -> Dict[str, Any]:
         """Returns the required user node"""
         response = self.get(f"/user_nodes/{user_node_record_id}")
         response.raise_for_status()
         return response.json()
 
-    def get_intent(self, intent_record_id: int) -> dict:
+    def get_user_nodes_full(self) -> List[Dict[str, Any]]:
+        """Returns all user nodes with full information"""
+        return [self.get_user_node(un["record_id"]) for un in self.get_user_nodes()]
+
+    def get_intent(self, intent_record_id: int) -> Dict[str, Any]:
         """Returns the intent matching the given id"""
         response = self.get(f"intents/{intent_record_id}")
         response.raise_for_status()
         return response.json()
 
-    def deploy_intent(self, intent_record_id: int):
+    def deploy_intent(self, intent_record_id: int) -> Dict[str, Any]:
         """Deploy the intent matching the given id"""
         response = self.put(f"intents/{intent_record_id}/deploy")
         response.raise_for_status()
         return response.json()
 
-    def update_entity(self, entity_record_id: int, entity: dict):
+    def update_entity(self, entity_record_id: int, entity: dict) -> Dict[str, Any]:
         """Update the entity matching the given id with the given entity object"""
         response = self.put(f"entities/{entity_record_id}", json=entity)
         response.raise_for_status()
         return response.json()
 
-    def get(self, url, **kwargs):
+    def get(self, url, **kwargs) -> requests.Response:
         """Perform a GET request using the Mindsay base URL"""
         return super().get(self.base_url + url, **kwargs)
 
