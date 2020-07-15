65477f7755c37c66afdcb34741846e333d011f09
Salim Aboubacar <salim@destygo.com>
2020-07-15 18:31:04 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mindsay-sdk",
-    version="1.0.0",
+    version="1.1.0",
     author="Mindsay tech team",
     author_email="tech@mindsay.com",
     description="Mindsay python client",
